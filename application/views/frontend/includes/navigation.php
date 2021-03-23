8b3a3f03ed7167fef094eb411dfca4978a2954ce
Saman Shahzadi <saman.shabir@gmail.com>
2021-03-23 17:49:23 +0400

    lang

--- application/views/frontend/includes/navigation.php
+++ application/views/frontend/includes/navigation.php
@@ -3,24 +3,24 @@ $lang = lang() == 'english' ? 'en' : 'ar';
 ?>
 <nav>
 <a class="toggle-wrap" href='javascript:;' onclick="toggleMenu(this)">
-    <span class="toggle-bar"><span>menu</span></span>
+    <span class="toggle-bar"><span><?= $this->lang->line('menu') ?></span></span>
 </a>
 <?= $breadcrumb ?? '' ?>
 </nav>
 <aside>
     <ul class='menuss'>
-        <li><a href="<?= base_url() ?>">Home</a></li>
-        <li class="dropdown"><a href="javascript:;">Products</a>
+        <li><a href="<?= base_url() ?>"><?= $this->lang->line('Home') ?></a></li>
+        <li class="dropdown"><a href="javascript:;"><?= $this->lang->line('Products') ?></a>
             <ul>
-                <li><a href="<?= base_url($lang . '/products/dates') ?>">Dates</a></li>
-                <li><a href="<?= base_url($lang . '/products/debes') ?>">Debes</a></li>
-                <li><a href="<?= base_url($lang . '/products/gifts') ?>">Gifts</a></li>
-                <li><a href="<?= base_url($lang . '/products') ?>">All</a></li>
+                <li><a href="<?= base_url($lang . '/products/dates') ?>"><?= $this->lang->line('dates') ?></a></li>
+                <li><a href="<?= base_url($lang . '/products/debes') ?>"><?= $this->lang->line('debes') ?></a></li>
+                <li><a href="<?= base_url($lang . '/products/gifts') ?>"><?= $this->lang->line('gifts') ?></a></li>
+                <li><a href="<?= base_url($lang . '/products') ?>"><?= $this->lang->line('All') ?></a></li>
             </ul>
         </li>
-        <li><a href="<?= base_url($lang . '/contact') ?>">Contact Us</a></li>
-        <li><a href="<?= base_url($lang . '/about') ?>">About Us</a></li>
-        <li><a href="<?= base_url($lang . '/cart') ?>">Basket</a></li>
+        <li><a href="<?= base_url($lang . '/contact') ?>"><?= $this->lang->line('contact-us') ?></a></li>
+        <li><a href="<?= base_url($lang . '/about') ?>"><?= $this->lang->line('about-us') ?></a></li>
+        <li><a href="<?= base_url($lang . '/cart') ?>"><?= $this->lang->line('basket') ?></a></li>
     
     
     </ul>
